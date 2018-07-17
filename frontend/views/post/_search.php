a4434431fe271951db058044b5477060b6a3625d
Vyacheslav Fisyuk <bspotf@gmail.com>
2018-07-17 15:05:23 +0300

    Post mvc added on front

---
+++ frontend/views/post/_search.php
@@ -0,0 +1,41 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\PostSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="post-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'author_id') ?>
+
+    <?= $form->field($model, 'title') ?>
+
+    <?= $form->field($model, 'content') ?>
+
+    <?= $form->field($model, 'category_id') ?>
+
+    <?php // echo $form->field($model, 'comment_status') ?>
+
+    <?php // echo $form->field($model, 'karma') ?>
+
+    <?php // echo $form->field($model, 'date') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ frontend/views/post/index.php
@@ -0,0 +1,70 @@
+<?php
+
+use common\models\User;
+use common\models\Post;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel frontend\models\PostSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Posts';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="post-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Post', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+
+                [
+                    'attribute' => 'id',
+                    'label' => 'User',
+                    'value' => function (Post $post) {
+                        $user = User::findOne($post->author_id);
+                        return ($user->username);
+                    },
+                    'filter' => ArrayHelper::map(
+                        User::find()->all(),
+                        'id',
+                        'username'),
+                ],
+                'author_id',
+                'title',
+//                'content',
+                [
+                    'attribute' => 'content',
+                    'value' => function (Post $post) {
+                        $length = 200;
+                        $str = substr($post->content, 0, $length);
+                        if (strlen($post->content) > $length) {
+                            $str .= '...';
+                        }
+                        return $str;
+                    },
+                    'contentOptions' => [
+                            'style' => "white-space: normal; max-width: 500px;"
+                    ],
+                ],
+
+                //'category_id',
+                //'comment_status',
+                //'karma',
+                //'date',
+
+                ['class' => 'yii\grid\ActionColumn'],
+            ],
+        ]);
+     ?>
+</div>
